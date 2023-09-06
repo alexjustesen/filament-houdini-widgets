ab1a241650fdc3c8a493b284f95b60ac2a36954b
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-09-06 09:24:35 -0400

    initial commit

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Filament\Widgets\BlogPostsChart;
+use App\Filament\Widgets\EventsChart;
+use App\Filament\Widgets\StatsOverview;
+use Filament\Actions\Action;
+use Filament\Notifications\Notification;
+use Filament\Pages\Dashboard as BasePage;
+
+class Dashboard extends BasePage
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.dashboard';
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('button')
+                ->label('Action button')
+                ->action(fn () => $this->doTheThing()),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            StatsOverview::class,
+        ];
+    }
+
+    protected function getFooterWidgets(): array
+    {
+        return [
+            BlogPostsChart::make(),
+            EventsChart::make(),
+        ];
+    }
+
+    protected function doTheThing(): void
+    {
+        Notification::make()
+            ->title('The things has been done!')
+            ->success()
+            ->send();
+    }
+}
