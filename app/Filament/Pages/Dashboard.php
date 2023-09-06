45b35ac747fe8bf85bf88f4408446b832241200d
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-09-06 09:33:56 -0400

    removed dashboard icon

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -11,8 +11,6 @@ use Filament\Pages\Dashboard as BasePage;
 
 class Dashboard extends BasePage
 {
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
-
     protected static string $view = 'filament.pages.dashboard';
 
     protected function getHeaderActions(): array
